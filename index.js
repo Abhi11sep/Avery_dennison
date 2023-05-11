fa59933c7f507bbe5561b8496c21e5b7df7b3a0b
Abhishek Singh <abhi.rocks.11.9.1997@gmail.com>
2023-05-11 11:22:39 +0530

    completed

--- index.js
+++ index.js
@@ -27,3 +27,26 @@ function topFunction() {
       })
   }
 
+  function submit_mob() {
+
+    let First_name = document.getElementById("first_name_mob").value;
+    let Last_name = document.getElementById("last_name_mob").value;
+    let Contact = document.getElementById("contact_mob").value;
+    let Email = document.getElementById("email_mob").value;
+    let Company = document.getElementById("company_mob").value;
+    let State = document.getElementById("state_mob").value;
+    let Vender = document.getElementById("vender_mob").value;
+    let Distributor = document.getElementById("distributor_mob").value;
+
+    if (First_name === "" || Last_name === "" || Contact == "" || Email == "" ||
+        Company == "" || State == "" || Vender == "" || Distributor == "") {
+        alert("Please Fill all the fields to before submitting")
+    } else {
+        alert("Submitted successfully")
+        let form=document.getElementById("form_mob")
+        form.style.display="none"
+        let n=document.getElementById("second_A_mob")
+        n.style.marginTop="-1150px"
+    }
+}
+
